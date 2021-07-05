3c11da5995a462276a96583540a72b8d357c0162
bprabhakar <prabhakar.bharat@gmail.com>
2021-07-05 17:53:30 +0530

    yolo commit

--- app.py
+++ app.py
@@ -3,70 +3,45 @@ import requests
 import json
 from PIL import Image
 from io import BytesIO
-import torch
-import clip as clip
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-st.markdown("# Zero Shot Image Classifier")
-st.markdown("""
-How this works is simple. You enter the URL of an image and a list of text labels that you want to classify the image for.
-The model will then assign scores to each text label - probability that the given image matches that particular text label.
-
-So, for example if you entered an image of an animal, you could enter the following text labels: 
-
-`dog, cat, tiger, bear`
-
-Or, if you wanted to predict the color of a shoe, you could enter:
-
-`red, blue, black`
-
-You get the idea. Pick any image and any set of text labels and see the magic. Be creative!
-
-""")
+import boto3
+import botocore
 
 
-@st.cache(hash_funcs={dict: lambda _: None})
-def load_models():
-    model, preprocess = clip.load("ViT-B/32", device=device)
-    return {"model": model, "preprocess": preprocess}
+def parse_lambda_response_payload(payload):
+    if payload is not None:
+        payload_as_str = payload.read().decode("utf-8")
+        return json.loads(payload_as_str)
+    else:
+        return {}
+
+
+def invoke_lambda(name, payload):
+    config = botocore.config.Config(read_timeout=900, connect_timeout=900, retries={"max_attempts": 0})
+    lambda_client = boto3.client(
+        "lambda",
+        region_name="us-east-2",
+        config=config,
+        aws_access_key_id=st.secrets["access_key"],
+        aws_secret_access_key=st.secrets["secret_key"],
+    )
+    r = lambda_client.invoke(FunctionName=name, InvocationType="RequestResponse", Payload=json.dumps(payload))
+    return parse_lambda_response_payload(r.get("Payload"))
 
 
 @st.cache
-def render_img_url(url, local=False):
-    if not local:
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content))
-    else:
-        img = Image.open(url)
+def render_img_url(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
     return img
 
 
-def predict(image, text_classes):
-    loader = load_models()
-    model = loader["model"]
-    preprocess = loader["preprocess"]
-    image = preprocess(image).unsqueeze(0).to(device)
-    text = clip.tokenize(text_classes).to(device)
-    with torch.no_grad():
-        image_features = model.encode_image(image)
-        text_features = model.encode_text(text)
-        logits_per_image, logits_per_text = model(image, text)
-        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-    preds = {
-        text_classes[i]: "{0:.0%}".format(probs[0][i])
-        for i in range(len(text_classes))
-    }
-    return preds
+# Visual App
 
-
-img_url = st.text_input('Enter the image URL')
+st.markdown("# ShopNet by mustard")
+img_url = st.text_input("Enter the image URL")
 if img_url:
     image = render_img_url(img_url)
     st.image(image, use_column_width=True)
-    raw_text_labels = st.text_input(
-        "Enter comma separated text classes to predict:")
-    if raw_text_labels:
-        labels = [x.strip() for x in raw_text_labels.split(",")]
-        preds = predict(image, labels)
-        preds
+    preds = invoke_lambda(st.secrets["lambda_name"], {"product": {"image_url": img_url}})
+    preds = json.loads(preds["body"])
+    preds
